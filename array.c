156e0f88fff5c8901f491c0e5d43d534d3fa7e69
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-09 10:40:43 +0600

    first commit

--- array.c
+++ array.c
@@ -5,14 +5,18 @@ int main()
     for (int i = 0; i < 5; i++)
     {
         scanf("%d", &a[i]);
- }
- for (int i = 0; i < 5; i++)
- {
-    printf("%d ", a[i]);
-// printf("%d ", &a[i]); for checking  the address
- }
- 
-    printf("%d ", a[2]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        a[0]+=5;
+    }
+    
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", a[i]);
+        // printf("%d ", &a[i]); for checking  the address
+    }
+
     return 0;
 }
 // 0 1 2 3 4
